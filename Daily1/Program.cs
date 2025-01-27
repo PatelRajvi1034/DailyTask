a87cf44d7b8d401fdcfb6eae599c8d7e0589134c
PatelRajvi1034 <rajvi.r.patel@bacancy.com>
2025-01-27 17:01:17 +0530

    Update Program.cs

--- Daily1/Program.cs
+++ Daily1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int number1 = int.Parse(Console.ReadLine());
+        int number2 = int.Parse(Console.ReadLine());
+        int number3 = int.Parse(Console.ReadLine());
+        Console.WriteLine("You entered: " + number2);
+    }
+}
